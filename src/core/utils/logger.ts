ff09d1d81707394db0fc3b716d4f8c0d0bd0bf29
F88\sangnt3 <sangnt3@f88.vn>
2025-08-13 16:35:49 +0700

    lession_19_log_to_file_winston

--- src/core/utils/logger.ts
+++ src/core/utils/logger.ts
@@ -1,11 +1,26 @@
 import winston from "winston";
 
 const Logger: winston.Logger = winston.createLogger({
-  transports: [new winston.transports.Console()],
+  transports: [
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    //
+    // - Write all logs with importance level of `info` or higher to `combined.log`
+    //   (i.e., fatal, error, warn, and info, but not trace)
+    //
+    new winston.transports.File({ filename: "logs/combined.log" }),
+  ],
   format: winston.format.combine(
     winston.format.colorize({ all: true }),
     winston.format.simple()
   ),
 });
+
+if (process.env.NODE_ENV !== "production") {
+  Logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
 
 export default Logger;

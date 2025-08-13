be077886c96d49eb1e4252f5126d3eb0e8b70f27
F88\sangnt3 <sangnt3@f88.vn>
2025-08-13 14:35:06 +0700

    lesson_13_14_connect_db_and_envfile

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Route } from "core/interfaces";
 import express from "express";
+import mongoose from "mongoose";
 
 class App {
   public app: express.Application;
@@ -9,6 +10,8 @@ class App {
     this.port = process.env.PORT || 5000;
 
     this.initializeRoutes(routes);
+
+    this.connectToDatabase();
   }
 
   private initializeRoutes(routes: Route[]) {
@@ -21,6 +24,20 @@ class App {
     this.app.listen(this.port, () => {
       console.log(`App listening on the port ${this.port}`);
     });
+  }
+
+  private connectToDatabase() {
+    // Replace with your MongoDB connection string
+    const mongoURI = `mongodb+srv://${process.env.UserId}:${process.env.Password}@clustertedu.p5viikc.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTedu`;
+
+    mongoose
+      .connect(mongoURI)
+      .then(() => {
+        console.log("MongoDB connected");
+      })
+      .catch((err) => {
+        console.error("MongoDB connection error:", err);
+      });
   }
 }
 export default App;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { IndexRoute } from "./modules/index";
 import App from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
 const routes = [new IndexRoute()];
 const app = new App(routes);
